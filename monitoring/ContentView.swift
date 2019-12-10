2a1cfe296c9391e214dadd4aa5e6f2d8549be3ed
aymericvanderkelen <aymeric62vanderkelen@gmail.com>
2019-12-10 11:01:39 +0100

    initial commit

---
+++ monitoring/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  monitoring
+//
+//  Created by Aymeric Vanderkelen on 10/12/2019.
+//  Copyright © 2019 Aymeric Vanderkelen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World").font(.title).foregroundColor(.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
